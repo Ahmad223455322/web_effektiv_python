bb89925bc57c41370a92340261daa2c17147cf37
Ahmad223455322 <ahmadzrzr97@gmail.com>
2022-02-22 09:01:20 +0100

    ny main

--- areas/customer/customerPages.py
+++ areas/customer/customerPages.py
@@ -15,7 +15,7 @@ customerBlueprint = Blueprint('customer', __name__)
 
 
 @customerBlueprint.route("/överföring", methods=['GET', 'POST'])
-# @roles_required("Admin")
+@roles_required("Admin")
 def överföring():
     
     form= Överförnig(request.form)
@@ -57,7 +57,7 @@ def överföring():
 
 
 @customerBlueprint.route("/insätning", methods=['GET', 'POST'])
-# @roles_required("Admin")
+@roles_required("Admin")
 def insätning():
   
     
@@ -100,7 +100,7 @@ def insätning():
 
 
 @customerBlueprint.route("/Kundbild", methods=['GET', 'POST'])
-# @roles_accepted("Admin","Cashier")
+@roles_accepted("Admin","Cashier")
 def kundbild():
     
     page=int(request.args.get('page','1'))   
@@ -126,7 +126,7 @@ def kundbild():
 
 
 @customerBlueprint.route("/kontobild", methods=['GET', 'POST'])
-# @roles_accepted("Admin","Cashier")
+@roles_accepted("Admin","Cashier")
 def kontobild():
     sortColumn = request.args.get('sortColumn','ID')
     sortOrder = request.args.get('sortOrder','asc')
@@ -141,7 +141,7 @@ def kontobild():
 
 
 @customerBlueprint.route("/transaktionerbild", methods=['GET', 'POST'])
-# @roles_accepted("Admin","Cashier")
+@roles_accepted("Admin","Cashier")
 def transaktionerbild():
     sortColumn = request.args.get('sortColumn','ID')
     sortOrder = request.args.get('sortOrder','asc')
